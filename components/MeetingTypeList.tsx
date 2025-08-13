861355a842a875272ff98e1ee0281bce97b75548
Muzamil Khan <113015136+Zaibten@users.noreply.github.com>
2025-08-13 15:12:14 +0500

    Update MeetingTypeList.tsx

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -103,7 +103,7 @@ const MeetingTypeList = () => {
       if (meetingState === 'isScheduleMeeting') {
         try {
           const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${call.id}`;
-          const response = await fetch('http://localhost:5000/send-scheduleemail', {
+          const response = await fetch('https://syncmeetserver.vercel.app/send-scheduleemail', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
